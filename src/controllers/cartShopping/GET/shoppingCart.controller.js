e92a7b9667066728d3937815a2e0171f4f392803
oscar bejarano <osc4r.yt@gmail.com>
2023-04-04 09:47:54 -0500

    reset3

--- src/controllers/cartShopping/GET/shoppingCart.controller.js
+++ src/controllers/cartShopping/GET/shoppingCart.controller.js
@@ -1,29 +1,16 @@
-const {CartShoppingService} = require('../../../services/cartShopping.service');
+const {
+  CartShoppingService,
+} = require("../../../services/cartShopping.service");
 
 const cartService = new CartShoppingService();
 const getShoppingCart = (req, res, next) => {
-    try {
-        const {UserId} = req.body;
-        const cartShopping = cartService.getCartShopping(UserId);
-        res.status(200).send(cartShopping);
-    }catch (e) {
-        next(e)
-    }
-    /*
-    const getShoppingCart = (req, res, next) => {
-    try {
-        const {UserId} = req.body;
-        const cartShopping = cartService.getCartShopping(UserId);
-        res.status(200).send(cartShopping);
-    }catch (e) {
-        next(e)
-    }
-    
-    
-    
+  try {
+    const { UserId } = req.body;
+    const cartShopping = cartService.getCartShopping(UserId);
+    res.status(200).send(cartShopping);
+  } catch (e) {
+    next(e);
+  }
+};
 
-}    
-
-*/
-
-module.exports = {getShoppingCart}
+module.exports = { getShoppingCart };
