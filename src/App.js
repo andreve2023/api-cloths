7fa44e1230d80345e36261a23242bfdc913ebca3
oscar bejarano <osc4r.yt@gmail.com>
2023-04-04 09:24:35 -0500

    cors2

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
