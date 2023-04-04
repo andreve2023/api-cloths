e17d95e3cb1e37b35d39921fd43a5d93a830fa86
oscar bejarano <osc4r.yt@gmail.com>
2023-04-04 09:26:59 -0500

    cors3

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ const {
   logError,
 } = require("./middlewares/errorHandlers");
 
-app.use(
-  cors({
-    allowedOrigins: ["*"],
-  })
-);
+// app.use(cors({
+//     allowedOrigins: ['http://localhost:3000'],
+//     allowedMethods: ['GET', 'POST'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+// }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
